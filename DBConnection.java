d203ddd7e93fbe18fc081dcdecdd8ceca00b990f
lakshyarajpoot07-debug <lakshyarajpoot.07@gmail.com>
2025-12-21 00:25:56 +0530

    Update DBConnection.java

--- DBConnection.java
+++ DBConnection.java
@@ -1,14 +1,1 @@
-class DBConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/eventdb";
-    private static final String USER = "root";
-    private static final String PASS = "";
 
-    public static Connection getConnection() {
-        try {
-            return DriverManager.getConnection(URL, USER, PASS);
-        } catch (Exception e) {
-            e.printStackTrace(); 
-            return null;
-        }
-    }
-}
